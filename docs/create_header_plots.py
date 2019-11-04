f33930812fe0811bd000def5fe64fb085a80108a
Vincent Lefoulon <vincent.lefoulon@free.fr>
2019-11-04 11:07:56 +0100

    Change header plot

--- docs/create_header_plots.py
+++ docs/create_header_plots.py
@@ -2,15 +2,20 @@ import os
 
 import ethik
 import json
+import keras
+from keras.datasets import mnist
+from keras import backend as K
 import lightgbm as lgb
+import numpy as np
 import pandas as pd
 from plotly.utils import PlotlyJSONEncoder
 from sklearn import model_selection
 
+HERE = os.path.dirname(os.path.abspath(__file__))
+
 
 def save_fig(fig, name):
-    here = os.path.dirname(os.path.abspath(__file__))
-    folder = os.path.join(here, "assets", "plots")
+    folder = os.path.join(HERE, "assets", "plots")
     path = os.path.join(folder, f"{name}.json")
 
     if not os.path.exists(folder):
@@ -28,7 +33,7 @@ def save_fig(fig, name):
         f.write(fig_json)
 
 
-if __name__ == "__main__":
+def setup_adult():
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
     names = [
         "age",
@@ -74,6 +79,44 @@ if __name__ == "__main__":
 
     explainer = ethik.ClassificationExplainer()
 
+    return explainer, X_test, y_pred, y_test
+
+
+def setup_mnist():
+    batch_size = 128
+    num_classes = 10
+    epochs = 12
+
+    img_rows, img_cols = 28, 28
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    if K.image_data_format() == "channels_first":
+        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+        input_shape = (1, img_rows, img_cols)
+    else:
+        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+        input_shape = (img_rows, img_cols, 1)
+
+    x_train = x_train.astype("float32")
+    x_test = x_test.astype("float32")
+    x_train /= 255
+    x_test /= 255
+
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+
+    y_pred = np.load(os.path.join(HERE, "..", "notebooks", "cache", "mnist.npy"))
+
+    explainer = ethik.ImageClassificationExplainer()
+
+    return explainer, x_test, y_pred, y_test
+
+
+if __name__ == "__main__":
+    explainer, X_test, y_pred, y_test = setup_adult()
     save_fig(
         explainer.plot_influence(X_test=X_test["education-num"], y_pred=y_pred),
         "header-plot-left",
@@ -81,7 +124,19 @@ if __name__ == "__main__":
 
     save_fig(
         explainer.plot_influence(
-            X_test=X_test[["education-num", "age"]], y_pred=y_pred
+            X_test=X_test[["age", "education-num", "hours-per-week"]], y_pred=y_pred
         ),
         "header-plot-right",
     )
+
+    """
+    explainer, X_test, y_pred, y_test = setup_mnist()
+    save_fig(
+        explainer.plot_influence(
+            X_test=X_test,
+            y_pred=y_pred,
+            cell_width=100
+        ),
+        "header-plot-right",
+    )
+    """
