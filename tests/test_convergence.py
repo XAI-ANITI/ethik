da75c5a8fc3f7b61916e1bde376a6d20f4b3d8f5
Max Halford <maxhalford25@gmail.com>
2019-11-07 17:23:30 +0100

    black

--- tests/test_convergence.py
+++ tests/test_convergence.py
@@ -12,12 +12,10 @@ def test_boston():
 
     boston = datasets.load_boston()
     X = pd.DataFrame(boston.data, columns=boston.feature_names)
-    y = pd.Series(boston.target, name='House price')
+    y = pd.Series(boston.target, name="House price")
 
     X_train, X_test, y_train, y_test = model_selection.train_test_split(
-        X, y,
-        shuffle=True,
-        random_state=42
+        X, y, shuffle=True, random_state=42
     )
 
     model = ensemble.RandomForestRegressor(n_estimators=10, max_depth=5)
@@ -30,9 +28,6 @@ def test_boston():
     with warnings.catch_warnings(record=True) as caught_warnings:
         warnings.simplefilter("ignore", category=ethik.warnings.ConstantWarning)
         explainer = ethik.RegressionExplainer(alpha=0.05, verbose=False)
-        explainer.explain_influence(
-            X_test=X_test,
-            y_pred=y_pred
-        )
+        explainer.explain_influence(X_test=X_test, y_pred=y_pred)
 
     assert len(caught_warnings) == 0
