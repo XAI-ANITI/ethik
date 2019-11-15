79cd1bb0e21c3343cb4679866627ad663b997a26
Max Halford <maxhalford25@gmail.com>
2019-11-15 16:39:41 +0100

    Clean docs/create_gallery

--- docs/create_gallery.py
+++ docs/create_gallery.py
@@ -80,9 +80,8 @@ def setup_adult():
 
 
 def setup_mnist():
-    batch_size = 128
+
     num_classes = 10
-    epochs = 12
 
     img_rows, img_cols = 28, 28
 
@@ -91,11 +90,9 @@ def setup_mnist():
     if K.image_data_format() == "channels_first":
         x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
         x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-        input_shape = (1, img_rows, img_cols)
     else:
         x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
         x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-        input_shape = (img_rows, img_cols, 1)
 
     x_train = x_train.astype("float32")
     x_test = x_test.astype("float32")
